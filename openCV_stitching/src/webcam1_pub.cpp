2a473116ebac224d447e2aadeda3d394f86e9a92
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-04 15:19:18 +0900

    fix error

--- openCV_stitching/src/webcam1_pub.cpp
+++ openCV_stitching/src/webcam1_pub.cpp
@@ -1,22 +1,21 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <image_transport/image_transport.h>
-#include <iostream>
-#include <vector>
+#include <sstream>
 #include <ros/ros.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <std_msgs/UInt8MultiArray.h>
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "webcam1_pub");
     
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::UInt8MultiArray>("camera1/image", 1);
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub = it.advertise("camera1/image", 1);
 
     cv::VideoCapture cap(0);
     cv::Mat frame;
-
+    sensor_msgs::ImagePtr msg;
     
     while(nh.ok())
     {
@@ -24,29 +23,10 @@ int main(int argc, char** argv)
 
         if(!frame.empty())
         {
-
-            // Encode, Decode image example            
-            std::vector<uchar> encode;
-            std::vector<int> encode_param;
-            
-            encode_param.push_back(CV_IMWRITE_JPEG_QUALITY);
-            encode_param.push_back(20);
-            
-            cv::imencode(".jpg", frame, encode, encode_param);
-            cv::Mat decode = cv::imdecode(encode, 1);
-            cv::imshow("decode", decode);
-
-            // Convert encoded image to ROS std_msgs format
-            std_msgs::UInt8MultiArray msgArray;
-            msgArray.data.clear();
-            msgArray.data.resize(encode.size());
-            std::copy(encode.begin(), encode.end(), msgArray.data.begin());
-
-            // Publish msg
-            pub.publish(msgArray);
-
-            cv::waitKey(1);
-          
+	    cv::imshow("webcam1", frame);
+	    msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+	    pub.publish(msg);
+	    cv::waitKey(1);
         }
 
         ros::spinOnce();
--- openCV_stitching/src/webcam_stitching.cpp
+++ openCV_stitching/src/webcam_stitching.cpp
@@ -1,24 +1,15 @@
 #include <ros/ros.h>
-#include <opencv2/opencv.hpp>
-#include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <vector>
-#include <std_msgs/UInt8MultiArray.h>
 #include "openCV_stitching/image_stitcher.h"
-
-void connectCallback(const std_msgs::UInt8MultiArray::ConstPtr& array) {
-	ROS_INFO("connecting");
-}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "webcam_stitching");
+  ros::init(argc, argv, "ImageStitcher", ros::init_options::AnonymousName);
+  
+  ros::NodeHandle nh_("~");
 
-  ros::NodeHandle nh;
-
-  ImageStitcher ic("camera1/image", "camera2/image", 10, true);
-  // pub = "/stitched_images/output"
-
-  ros::spin();
+  while(true) {
+  	ImageStitcher ic("camera1/image", "camera2/image", 10, true);
+  	// pub = "/stitched_images/output"
+  	ros::spinOnce();
+  }
 }
